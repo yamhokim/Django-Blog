e02d2f5162e1aa3209320fc78e834e13d825e912
yamhokim <yoonho.luke.kim@gmail.com>
2024-11-24 15:30:33 -0500

    created models for questions and choices, created views or the poll page and results, created associated templates for the different views

--- polls/models.py
+++ polls/models.py
@@ -16,7 +16,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=8)
+    votes = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return self.choice_text
